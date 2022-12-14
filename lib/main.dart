c8864623364fff2eb1a2d00678e61d3c0329c631
jadjer <jadjer@ya.ru>
2022-12-14 22:40:52 +0300

    feat: update project

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,21 @@
+/*
+ * Copyright 2022 Pavel Suprunov
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import 'package:flutter/material.dart';
+import 'package:ride2online/themes.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,20 +27,12 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    AppTheme theme = AppTheme.fromType(ThemeType.Orange_Dark);
+    ThemeData materialTheme = theme.toThemeData();
+
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
+      theme: materialTheme,
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
